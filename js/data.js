5b61f018c2b7055ed6f2f0f67e77d6a8f075b65d
khalil ait ammar <ait.ammar.khalil@gmail.com>
2016-04-16 09:36:56 +0200

    mise en place de framework bootstrap/angular/jquery

---
+++ js/data.js
@@ -0,0 +1,22 @@
+[
+    {
+        "name": "khalil",
+        "mp": "ait"
+    },
+    {
+        "name": "aman",
+        "mp": "ait"
+    },
+    {
+        "name": "ahmed",
+        "mp": "ait"
+    },
+    {
+        "name": "mohamed",
+        "mp": "ait"
+    },
+    {
+        "name": "yesine",
+        "mp": "ait"
+    }
+]
